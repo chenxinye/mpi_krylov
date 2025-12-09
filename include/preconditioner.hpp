b227fd653af31e9b654ea318e67e644b611f150b
chenxinye <cclcquant@yahoo.com>
2025-12-09 15:56:37 +0100

    fix precondtiioner

--- include/preconditioner.hpp
+++ include/preconditioner.hpp
@@ -1,14 +1,32 @@
 #ifndef PRECONDITIONER_HPP
 #define PRECONDITIONER_HPP
+
 #include <vector>
 #include "matrix.hpp"
 
-class Preconditioner {
-public:
-    // supply A in constructor for setup; apply: z = M^{-1} r (approx)
+struct Preconditioner {
     virtual void apply(const std::vector<double>& r_local, std::vector<double>& z_local) = 0;
-    virtual long long nnz_after() const { return -1; } // optionally report nnz of factor/approx
     virtual ~Preconditioner() = default;
+};
+
+struct JacobiPrecond : public Preconditioner {
+    std::vector<double> inv_diag;
+
+    JacobiPrecond(const CSRMatrix& A) {
+        inv_diag.resize(A.nrows);
+        for (int i = 0; i < A.nrows; ++i) {
+            inv_diag[i] = 0.0;
+            for (int j = A.row_ptr[i]; j < A.row_ptr[i+1]; ++j) {
+                if (A.col_idx[j] == i) inv_diag[i] = 1.0 / A.values[j];
+            }
+        }
+    }
+
+    void apply(const std::vector<double>& r_local, std::vector<double>& z_local) override {
+        int n = r_local.size();
+        z_local.resize(n);
+        for (int i = 0; i < n; ++i) z_local[i] = inv_diag[i] * r_local[i];
+    }
 };
 
 #endif
